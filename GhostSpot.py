feadca9f3a9fe111971a598e90214abe3097fcf4
Ghost <taylerobin@gmail.com>
2024-03-13 01:42:00 -0600

    cleaner output, better handling of trans_sesh corutines, and typing fixees

--- GhostSpot.py
+++ GhostSpot.py
@@ -3,6 +3,7 @@ __author__ = 'Ghost Glitch'
 
 import asyncio
 import io
+from typing import Any
 from winsdk.windows.media.control \
     import GlobalSystemMediaTransportControlsSessionManager as TCSManager
 from winsdk.windows.media.control \
@@ -16,13 +17,14 @@ from PIL import Image
 async def get_media_info() -> list:
     """ Returns a list of all active media as dicts of thier properties. """
     # Gets a list of all active TCS objects
-    sessions= list((await TCSManager.request_async()).get_sessions())
+    sessions= list((await TCSManager.request_async()).get_sessions()) # type: ignore
     # Returns a list of dictionaries of media info properties for each active
         # session, unless none can be found.
-    return [await trans_sesh(sesh) for sesh in sessions] if sessions else []
+    coroutines = [trans_sesh(sesh) for sesh in sessions]
+    return await asyncio.gather(*coroutines) if sessions else []
 
 
-async def trans_sesh(sesh: TCS) -> dict:
+async def trans_sesh(sesh: TCS) -> dict[str, Any]:
     """ Translates a Windows TCS object into a more Python friendly dictionary.
     Returns:
         A rougly organized dictionary of the TCS object's attributes. 
@@ -36,34 +38,39 @@ async def trans_sesh(sesh: TCS) -> dict:
         'album_title': props.album_title,
         'album_artist': props.album_artist,
         # Converts the genres property to a Python list.
-        'genres': list(props.genres),
+        'genres': list(props.genres), # type: ignore
         # Converts the thumbnail property to a PIL image.
-        'thumbnail': await ref_to_img(props.thumbnail),
+        'thumbnail': await ref_to_img(props.thumbnail), # type: ignore
         'track_number': props.track_number,
         'album_track_count': props.album_track_count,
-        'playback_type': props.playback_type,
+        'playback_type': props.playback_type.value, # type: ignore
         'subtitle': props.subtitle
     }
     return transed_sesh
 
 
-async def ref_to_img(stream_ref: IRandomAccessStreamReference) -> Image:
+async def ref_to_img(stream_ref: IRandomAccessStreamReference) -> Image.Image:
     """ Converts an IRandomAccessStreamReference object to a PIL image. """    # Opens a stream from the reference
     stream = await stream_ref.open_read_async()
     img_buffer = Buffer(stream.size)
     image_bytes = bytearray(stream.size)
     # Reads data from the stream into the buffer 
-    await stream.read_async(img_buffer, img_buffer.capacity, \
-                            InputStreamOptions.READ_AHEAD)
+    await stream.read_async(img_buffer, img_buffer.capacity, InputStreamOptions.READ_AHEAD) # type: ignore
     # Reads data from the buffer into the image_bytes bytearray.
-    DataReader.from_buffer(img_buffer).read_bytes(image_bytes)
-    # Converts the bytearray to a PIL image and returns it.  
-    return Image.open(io.BytesIO(image_bytes))   
+    DataReader.from_buffer(img_buffer).read_bytes(image_bytes) # type: ignore
+    # Converts the bytearray to a PIL image and returns it.
+
+    return Image.open(io.BytesIO(image_bytes))
 
 
 # only run if the module is run directly.
 if __name__ == '__main__':
     sessions = asyncio.run(get_media_info())
     if sessions:
-         print (sessions)
-         [sesh['thumbnail'].show() for sesh in sessions]
+        print(sessions)
+        for i, sesh in enumerate(sessions):
+            print (f'\nMedia {i+1}:')
+            print(sesh)
+            sesh['thumbnail'].show()
+    else:
+        print ( 'No active media found.')
