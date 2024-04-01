e48571ec4639e14f9a8ea48fe6864a02bfb47a1c
Ghost <taylerobin@gmail.com>
2024-04-01 03:06:02 -0600

    minor changes to testing to stay in sync

--- Tests/test_GhostSpot.py
+++ Tests/test_GhostSpot.py
@@ -1,11 +1,9 @@
 
-import sys
-sys.path.append("..") # Adds higher directory to python modules path.
+
 import pytest
 from PIL import Image
 from ..GhostSpot import *
 from ..GhostSpot import PlaybackType as PT
-from ..GhostSpot import Gcon as CV
 import asyncio
 from pytest_mock import mocker
 from winsdk.windows.media.control \
@@ -31,7 +29,7 @@ def tcs_mock(mocker):
     props_mock.thumbnail = None
     props_mock.track_number = 1
     props_mock.album_track_count = 10
-    props_mock.playback_type = PT.UNKNOWN
+    props_mock.playback_type = MPT.MUSIC
     props_mock.subtitle = "Test Subtitle"
 
     # Mock the try_get_media_properties_async method
@@ -42,40 +40,40 @@ def tcs_mock(mocker):
 def test_convert_to_PlaybackType():
     with pytest.warns() as Warn_list:
         # From MPT
-        assert PT.from_Any(MPT.UNKNOWN) == PT.UNKNOWN
-        assert PT.from_Any(MPT.MUSIC) == PT.AUDIO
-        assert PT.from_Any(MPT.VIDEO) == PT.VIDEO
-        assert PT.from_Any(MPT.IMAGE) == PT.IMAGE
+        assert GhostConverters.to_PT(MPT.UNKNOWN) == PT.UNKNOWN
+        assert GhostConverters.to_PT(MPT.MUSIC) == PT.AUDIO
+        assert GhostConverters.to_PT(MPT.VIDEO) == PT.VIDEO
+        assert GhostConverters.to_PT(MPT.IMAGE) == PT.IMAGE
         # From int
-        assert PT.from_Any(0) == PT.UNKNOWN
-        assert PT.from_Any(1) == PT.AUDIO
-        assert PT.from_Any(2) == PT.VIDEO
-        assert PT.from_Any(3) == PT.IMAGE
+        assert GhostConverters.to_PT(0) == PT.UNKNOWN
+        assert GhostConverters.to_PT(1) == PT.AUDIO
+        assert GhostConverters.to_PT(2) == PT.VIDEO
+        assert GhostConverters.to_PT(3) == PT.IMAGE
         assert len(Warn_list) == 0
-        assert PT.from_Any(4) == PT.UNKNOWN
+        assert GhostConverters.to_PT(4) == PT.UNKNOWN
         assert len(Warn_list) == 1
         # From str
-        assert PT.from_Any("UNKNOWN") == PT.UNKNOWN
-        assert PT.from_Any("AUDIO") == PT.AUDIO
-        assert PT.from_Any("VIDEO") == PT.VIDEO
-        assert PT.from_Any("IMAGE") == PT.IMAGE
-        assert PT.from_Any("audio") == PT.AUDIO
-        assert PT.from_Any("vIdEo") == PT.VIDEO
+        assert GhostConverters.to_PT("UNKNOWN") == PT.UNKNOWN
+        assert GhostConverters.to_PT("AUDIO") == PT.AUDIO
+        assert GhostConverters.to_PT("VIDEO") == PT.VIDEO
+        assert GhostConverters.to_PT("IMAGE") == PT.IMAGE
+        assert GhostConverters.to_PT("audio") == PT.AUDIO
+        assert GhostConverters.to_PT("vIdEo") == PT.VIDEO
         assert len(Warn_list) == 1
-        assert PT.from_Any("") == PT.UNKNOWN
-        assert PT.from_Any("XYZ") == PT.UNKNOWN
+        assert GhostConverters.to_PT("") == PT.UNKNOWN
+        assert GhostConverters.to_PT("XYZ") == PT.UNKNOWN
         assert len(Warn_list) == 3
         # From PT
-        assert PT.from_Any(PT.AUDIO) == PT.AUDIO
+        assert GhostConverters.to_PT(PT.AUDIO) == PT.AUDIO
         assert len(Warn_list) == 3
         # From None
-        assert PT.from_Any(None) == PT.UNKNOWN
+        assert GhostConverters.to_PT(None) == PT.UNKNOWN
         # From other
-        assert PT.from_Any(3.14) == PT.UNKNOWN
-        assert PT.from_Any([1, 2, 3]) == PT.UNKNOWN
+        assert GhostConverters.to_PT(3.14) == PT.UNKNOWN
+        assert GhostConverters.to_PT([1, 2, 3]) == PT.UNKNOWN
         assert len(Warn_list) == 6
         with pytest.raises(TypeError):
-            PT.from_Any(1, 2)
+            GhostConverters.to_PT(1, 2)
     
 
 def test_PySesh_default_vals():
@@ -83,21 +81,32 @@ def test_PySesh_default_vals():
     assert py_sesh.title == 'Unknown Title'
     assert py_sesh.artist == 'Unknown Artist'
     assert py_sesh.album_title == 'Unknown Album'
-    assert py_sesh.album_artist == ''
+    assert py_sesh.album_artist == None
     assert py_sesh.genres == ()
-    assert isinstance(py_sesh.thumbnail, Image.Image)
+    assert py_sesh.thumbnail is ERROR_THUMB
     assert py_sesh.track_number == 0
     assert py_sesh.album_track_count == 0
     assert py_sesh.playback_type == PT.UNKNOWN
-    assert py_sesh.subtitle == ''
+    assert py_sesh.subtitle == None
 
-def test_PySesh_bad_vals():
-    session = PySession(title="", artist="", album_title="", album_artist="", genres=(), track_number=-5, album_track_count=np.int16(5), playback_type=0, subtitle="")
-    print ("done")
+def test_PySesh_empty_vals():
+    py_sesh = PySession(title="", artist="", album_title="", album_artist="", genres=(), track_number=0, album_track_count=0, playback_type=0, subtitle="")
+    print (repr(py_sesh))
+    assert py_sesh.title == 'Unknown Title'
+    assert py_sesh.artist == 'Unknown Artist'
+    assert py_sesh.album_title == 'Unknown Album'
+    assert py_sesh.album_artist == None
+    assert py_sesh.genres == ()
+    assert py_sesh.thumbnail is ERROR_THUMB
+    assert py_sesh.track_number == 0
+    assert py_sesh.album_track_count == 0
+    assert py_sesh.playback_type == PT.UNKNOWN
+    assert py_sesh.subtitle == None
+
+
 
 @pytest.mark.asyncio
 async def test_create_PySesh_from_TCS(tcs_mock, mocker):
-
     # Call the from_TCS_async method to create a PySession object
     py_sesh = await PySession.from_TCS_async(tcs_mock)
 
@@ -110,5 +119,5 @@ async def test_create_PySesh_from_TCS(tcs_mock, mocker):
     assert isinstance(py_sesh.thumbnail, Image.Image)
     assert py_sesh.track_number == 1
     assert py_sesh.album_track_count == 10
-    assert py_sesh.playback_type == PT.UNKNOWN
+    assert py_sesh.playback_type == PT.AUDIO
     assert py_sesh.subtitle == "Test Subtitle"
